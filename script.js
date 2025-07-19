d2c17f815530715d8f5a9b70fd34f5aa278fa96f
Twinsland <samdamado@gmail.com>
2025-07-20 01:17:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -72,16 +72,16 @@ document.getElementById("ville-select").addEventListener("change", function () {
 // Les musiques
 const musiques = [
   {
-    titre: "Sagbohan Danialou - Mystique",
-    fichier: "assets/musics/mystique.mp3",
+    titre: "Sagbohan Danialou - Djidjoho",
+    fichier: "assets/musics/sagbohan1.mp3",
   },
   {
     titre: "Angélique Kidjo - Agolo",
-    fichier: "assets/musics/agolo.mp3",
+    fichier: "assets/musics/kidjo1.mp3",
   },
   {
-    titre: "First King - Vibe du Bénin",
-    fichier: "assets/musics/vibe-benin.mp3",
+    titre: "First King - Bon Choix",
+    fichier: "assets/musics/firstking1.mp3",
   }
 ];
 
